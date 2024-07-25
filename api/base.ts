cbbacec082ed90e393f6e65121fba6fa99bbaefc
funnyJack <554517318@qq.com>
2024-07-25 17:37:05 +0800

    加上判断当前环境方法的注释，以后会用到

--- api/base.ts
+++ api/base.ts
@@ -1,5 +1,6 @@
 // export const alibabaUrl="http://alb-ci1p69oty1wtv6wjxl.cn-hangzhou.alb.aliyuncs.com/api"
 // export const baseUrl = "http://localhost:8080/api"
+// wx.getAccountInfoSync().miniProgram.envVersion == "release"
 export const baseUrl = "http://alb-ci1p69oty1wtv6wjxl.cn-hangzhou.alb.aliyuncs.com/api"
 export const userInfoPath = "/userInfos"
 export const loginPath = userInfoPath + "/login"
